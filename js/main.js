a021747c6a3bdb5b61b86cc7a4de0407aa00569c
simona <simonaulloa@gmail.com>
2017-07-13 12:59:42 -0400

    inicializando

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-/* Holiii acá va tu código también */
+/*  Plugin menu desplegable */
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed
+    }
+  );
